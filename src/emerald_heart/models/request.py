89706dde8ff8b9b3c08ff5ee0aeb900586941486
Brant Watson <oldspiceap@gmail.com>
2026-07-27 17:11:44 -0500

    Fix selection visuals

--- src/emerald_heart/models/request.py
+++ src/emerald_heart/models/request.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import logging
 import uuid
+from collections import OrderedDict
+from typing import cast
 
 from django.db import models
 
@@ -33,6 +35,15 @@ class Request(BaseMixin, models.Model):
     @property
     def display_name(self) -> str:
         return f"{self.source_user.display_name} --> {self.dest_user.display_name}"
+
+    @property
+    def display_dict(self) -> OrderedDict[str, object]:
+        data = OrderedDict()
+        data["Created"] = self.created.strftime("%Y-%d-%m")
+        data["From User"] = self.source_user.display_name
+        data["To User"] = self.dest_user.display_name
+        LOG.debug("Data: %s", data)
+        return cast(OrderedDict[str, object], data)
 
     class Meta:
         """Meta information about the model."""
